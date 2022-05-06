c9481933da3b72231503db3e4cd5521308d4483c
hector <hpulido1021@gmail.com>
2022-05-05 20:05:16 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+
+const getDogImgs = () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((res) => res.json())
+    .then((dogImgData) => {
+        const dogImgContainer = document.querySelector("#dog-image-container")
+    
+        dogImgData.message.forEach((imgUrl) => {
+            const dogImg = document.createElement('img')
+            dogImg.src = imgUrl
+            dogImgContainer.append(dogImg)
+        })
+    })
+}
